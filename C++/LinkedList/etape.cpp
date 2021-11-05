40e787f7252d58dc5409cb2df7110297e9a8df97
Victor <78042182+0x0D15EA5Exd@users.noreply.github.com>
2021-11-05 18:38:24 +0100

    Update etape.cpp

--- C++/LinkedList/etape.cpp
+++ C++/LinkedList/etape.cpp
@@ -1,21 +1,65 @@
+#pragma warning (disable : 4996)
 #include "etape.h"
 #include <iostream>
+#include <string.h>
 #include <stdio.h>
-
+using namespace std;
+EtapeTrain* head = nullptr;
 
 EtapeTrain::EtapeTrain()
 {
 	std::cout << "Ctor EtapeTrain" << std::endl;
-	printf("0x%p \n", (void*)&precednte);
-	printf("0x%p \n", (void*)&suivante);
+	printf("0x%p \n", (void*)&precedent);
+	printf("0x%p \n", (void*)&suivant);
 }
 
-EtapeTrain* EtapeTrain::add(Date tmp, const char ville[LONGEUR_MOT])
+void EtapeTrain::add(Date tmp, const char station[LONGEUR_MOT], const char ville[LONGEUR_MOT], unsigned int k, arrivalTime arrival)
 {
 	EtapeTrain* Etape = new EtapeTrain;
-	Etape->date.jour = tmp.mois;
+	Etape->date.mois = tmp.mois;
+	Etape->date.jour = tmp.jour;
+	Etape->date.annee = tmp.annee;
+	Etape->boardingTime = k;
+	Etape->departureTime.hour = arrival.hour;
+	Etape->departureTime.minutes = arrival.minutes;
+	copy(Etape->ville, ville, LONGEUR_MOT);
+	Etape->setStation(station);
+	addToEnd(Etape, &head);
 
-	return Etape;
+}
+
+void EtapeTrain::setStation(const char* Station)
+{
+	copy(this->station, Station, LONGEUR_MOT);
+}
+
+void EtapeTrain::setArrivalTime(arrivalTime Tmp) const
+{
+	//
+}
+
+void EtapeTrain::copy(char* dst, const char* src, int max)
+{
+	int end = strlen(src);
+	if (end > max) end = max;
+	strncpy(dst, src, max);
+	dst[end] = '\0';
+}
+
+void EtapeTrain::addToEnd(EtapeTrain* Etape, EtapeTrain** head)
+{
+	EtapeTrain* last = *head;
+	if (*head)
+	{
+		Etape->precedent = nullptr;
+		*head = Etape;
+		return;
+	}
+
+	while (last->suivant != nullptr)
+		last->suivant = last->suivant;
+	last->suivant = Etape;
+	Etape->precedent = last;
 }
 
 EtapeTrain::~EtapeTrain()
@@ -27,6 +71,6 @@ EtapeTrain::~EtapeTrain()
 /******
 istream& operator>>(istream& i, EtapeTrain etapeT)
 {
-	
+
 	return i;
 }*/
