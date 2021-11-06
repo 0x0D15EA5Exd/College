aa09baa7c152b0b0d3a360635ec2e1cdbb78cb99
Victor <78042182+0x0D15EA5Exd@users.noreply.github.com>
2021-11-07 00:03:36 +0100

    Update etape.h

--- C++/LinkedList/etape.h
+++ C++/LinkedList/etape.h
@@ -2,7 +2,6 @@
 #include <iostream>	
 using namespace std;
 
-
 const int LONGEUR_MOT = 16;
 
 struct Date
@@ -40,12 +39,45 @@ public:
 	friend istream& operator >> (istream& i, Etape& tmp);
 	friend ostream& operator << (ostream& o, const Etape& );
 	~Etape();
-private:
+protected:
 	unsigned int boardingTime;
 	char station[LONGEUR_MOT];
 	arrivalTime departureTime;
 };
 
+//void append(Etape** startRef);
+//void printList(Etape* etapeT);
 
-void append(Etape** startRef);
-void printList(Etape* etapeT);
+class EtapeExtended : public Etape
+{
+public:
+	void add(EtapeExtended** startRef);
+private:
+	EtapeExtended* next();
+};
+
+/*
+void append(Etape** startRef)
+{
+	Etape* new_node = new Etape();
+	EtapeStr* last = reinterpret_cast<EtapeStr*>(*startRef);
+	std::cin >> *new_node;
+	new_node->next = nullptr;
+
+	if (*startRef == nullptr)
+	{
+		new_node->next = nullptr;
+		*startRef = new_node;
+		return (void)"Very SUS ";
+	}
+
+	while (last->next != nullptr)
+	{
+		last = last->next;
+	}
+
+	//	last->next = new_node;
+	new_node->previous = last;
+	return(void)"k";
+}
+*/
