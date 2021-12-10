1af687ff84cf6ed6b0fd6f669ffee069712dcff5
Victor <78042182+0x0D15EA5Exd@users.noreply.github.com>
2021-12-10 16:32:20 +0100

    Update Temperature.hpp

--- C++/Simulacre/Temperature.hpp
+++ C++/Simulacre/Temperature.hpp
@@ -1,25 +1,47 @@
-//
-//  Temperature.hpp
-//  Test
-//
-//  Created by Victor on 03/12/2021.
-//
+/*!
+ * \file Temperature.h++
+ * \author 0xdeadc0de
+ * \version 1.0-stable-release
+ */
 
 #ifndef Temperature_hpp
 #define Temperature_hpp
 
 #include "Capteur.hpp"
 
-
+/*! \class Temperature
+ *  \brief Classe Temperature, herite de Capteur.
+ *
+ *  La classe gere les différentes constantes en rapport avec l'oxygene, ainsi que les relevés.
+ */
 class Temperature : public Capteur {
     
 public:
+    /*!
+     * \brief Constructeur
+     *  Constructeur de la classe Temperature
+     */
     Temperature();
-   // ~Temperature();
+    /*!
+     *  \brief Destructeur
+     *
+     *  Destrcuteur de la classe Temperature
+     */
+    ~Temperature();
+    
+    /*!
+     *  \brief Lecture des valeurs
+     *  Methode qui permete de lire les constantes de à la classe Temperature.
+     *  \return True si les valeurs sont lu sans rencontrer de problèmes, false sinon.
+     */
     bool lireCapteur();
+    /*!
+     *  \brief Accesseur vers t.
+     *  \return Un reel, la valeur de T..
+     */
     float GetT();
 private:
-    float T;
+    float T; /*!< Valeur  T*/
 };
 
 
