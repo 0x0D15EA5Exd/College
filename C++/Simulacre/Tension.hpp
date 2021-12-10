4c1ef62dbdb509578ebdf7a086ae8becf76088c0
Victor <78042182+0x0D15EA5Exd@users.noreply.github.com>
2021-12-10 16:30:47 +0100

    Added commentary

--- C++/Simulacre/Tension.hpp
+++ C++/Simulacre/Tension.hpp
@@ -1,29 +1,51 @@
-//
-//  Tension.hpp
-//  Test
-//
-//  Created by Victor on 03/12/2021.
-//
+#include "Capteur.hpp"
+#include "Tension.hpp"
+#include <iostream>
+#include <random>
 
-#ifndef Tension_hpp
-#define Tension_hpp
+/*!
+ *  \brief Générateur de nombres aléatoires entiers
+ *
+ *  Générateur de nombres aléatoires entiers uniformément distribués qui produit des nombres aléatoires non déterministes.
+ */
+std::random_device rdTension;
 
-class Tension : public Capteur {
-    
-public:
-    int iDebug;
-    Tension();
-  //  ~Tension();
-    bool lireCapteur();
-    int GetTensys();
-    int GetTendia();
-    int GetTenP();
-    
-private:
-    int Tensys;
-    int Tendia;
-    int TenP;
-    
-};
+/// \fn static std::mt19937 engienRandomTension(rdTension())
+/// \brief Moteur de nombres aléatoires basé sur l'algorithme Mersenne Twister.
+/// \param rdTension seed
+///
+static std::mt19937 engineRandomTension(rdTension());
 
-#endif /* Tension_hpp */
+
+/// \fn static std::uniform_real_distribution <> disTen--- ( int, int)
+/// \brief Produit des valeurs aléatoires à virgule flottante x, uniformément distribuées sur l'intervalle [a,b).
+/// \param int,int
+static std::uniform_real_distribution<> disTendya(30,200), distTensys(90, 180), disTenP(30, 40);
+
+
+Tension::Tension() {
+    std::cout << "Ctor Tension" << std::endl;
+}
+int Tension::GetTenP() {
+    return this->TenP;;
+}
+
+int Tension::GetTendia(){
+    return this->Tendia;
+}
+
+int Tension::GetTensys(){
+    return this->Tensys;
+}
+
+bool Tension::lireCapteur() {
+    this->Tensys = (int)distTensys(engineRandomTension);
+    this->Tendia = (int)disTendya(engineRandomTension);
+    this->TenP = (int)disTenP(engineRandomTension);
+    return true;
+}
+
+Tension::~Tension() noexcept { 
+    std::cout << " Dtor Tensions " << std::endl;
+}
+
