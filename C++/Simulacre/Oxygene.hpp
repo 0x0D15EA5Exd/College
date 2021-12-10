e044218b996177bbb5507d035fb9126dbf956e60
Victor <78042182+0x0D15EA5Exd@users.noreply.github.com>
2021-12-10 16:31:43 +0100

    Update Oxygene.hpp

--- C++/Simulacre/Oxygene.hpp
+++ C++/Simulacre/Oxygene.hpp
@@ -1,28 +1,57 @@
-//
-//  Oxygene.hpp
-//  Test
-//
-//  Created by Victor on 03/12/2021.
-//
-
 #ifndef Oxygene_hpp
 #define Oxygene_hpp
 
 
-#include "Capteur.hpp"
+/*!
+ * \file Capteur.h++
+ * \author 0xdeadc0de
+ * \version 1.0-stable-release
+ */
 
+#include "Capteur.hpp"
+/*! \class Oxygene
+ *  \brief Classe Oxygene, herite de Capteur.
+ *
+ *  La classe gere les différentes constantes en rapport avec l'oxygene, ainsi que les relevés.
+ */
 class Oxygen : public Capteur {
     
 public:
+    /*!
+     * \brief Constructeur
+     *  Constructeur de la classe Oxygene
+     */
     Oxygen();
-    //~Oxygen();
+    /*!
+     *  \brief Destructeur
+     *
+     *  Destrcuteur de la classe Oxygene
+     */
+    ~Oxygen();
+    
+    /*!
+     *  \brief Lecture des valeurs
+     *  Methode qui permete de lire les constantes de à la classe Oxygene.
+     *  \return True si les valeurs sont lu sans rencontrer de problèmes, false sinon.
+     */
     bool lireCapteur();
-    int GetOx();
+    
+    /*!
+     *  \brief Acceseur vers Ox
+     *  \return Un reel, la valeur contenue dans Ox.
+     */
+    
+    
+    float GetOx();
+    /*!
+     *  \brief Accesseur vers Po.
+     *  \return Un entier, la valeur de Po.
+     */
     int GetPo();
     
 private:
-    float Ox;
-    int PO;
+    float Ox; /*!< Valeur oxygene*/
+    int PO; /*!< Valeur pouls*/
 };
 
 
