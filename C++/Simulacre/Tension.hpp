42d78e5d53bef5738684019bf57840bdf2f7d96c
Victor <78042182+0x0D15EA5Exd@users.noreply.github.com>
2021-12-13 08:44:03 +0100

    Update Tension.hpp

--- C++/Simulacre/Tension.hpp
+++ C++/Simulacre/Tension.hpp
@@ -1,51 +1,60 @@
-#include "Capteur.hpp"
-#include "Tension.hpp"
-#include <iostream>
-#include <random>
-
 /*!
- *  \brief Générateur de nombres aléatoires entiers
- *
- *  Générateur de nombres aléatoires entiers uniformément distribués qui produit des nombres aléatoires non déterministes.
+ * \file Tension.h++
+ * \author 0xdeadc0de
+ * \version 1.0-stable-release
  */
-std::random_device rdTension;
 
-/// \fn static std::mt19937 engienRandomTension(rdTension())
-/// \brief Moteur de nombres aléatoires basé sur l'algorithme Mersenne Twister.
-/// \param rdTension seed
-///
-static std::mt19937 engineRandomTension(rdTension());
+#ifndef Tension_hpp
+#define Tension_hpp
+/*!
+ *  \class Tension.
+ *  \brief Classe Tension, herite de capteur.
+ *
+ *  La classe gere les différentes constantes en rapport avec la tension.
+ *
+ */
+class Tension : public Capteur {
+    
+public:
+    int iDebug;
+    /*!
+     * \brief Constructeur
+     *  Constructeur de la classe Tension
+     */
+    Tension();
+    /*!
+     *  \brief Destructeur
+     *
+     *  Destrcuteur de la classe Tension
+     */
+    virtual ~Tension();
+    /*!
+     *  \brief Lecture des valeurs
+     *  Methode qui permete de lire les constantes de à la classe Tension.
+     *  \return True si les valeurs sont lu sans rencontrer de problèmes, false sinon.
+     */
+    bool lireCapteur();
+    /*!
+     *  \brief Acceseur vers Tensys.
+     *  \return Un reel, la valeur contenue dans Tensys.
+     */
+    int GetTensys();
+    /*!
+     *  \brief Acceseur vers Tendia.
+     *  \return Un reel, la valeur contenue dans Tendia.
+     */
+    int GetTendia();
+    /*!
+     *  \brief Acceseur vers Tendia.
+     *  \return Un reel, la valeur contenue dans TenP.
+     */
+    int GetTenP();
+    
+private:
+    int Tensys; /*!< Valeur Tensys*/
+    int Tendia; /*!< Valeur Tendia*/
+    int TenP;/*!< Valeur TenP*/
+    
+};
 
-
-/// \fn static std::uniform_real_distribution <> disTen--- ( int, int)
-/// \brief Produit des valeurs aléatoires à virgule flottante x, uniformément distribuées sur l'intervalle [a,b).
-/// \param int,int
-static std::uniform_real_distribution<> disTendya(30,200), distTensys(90, 180), disTenP(30, 40);
-
-
-Tension::Tension() {
-    std::cout << "Ctor Tension" << std::endl;
-}
-int Tension::GetTenP() {
-    return this->TenP;;
-}
-
-int Tension::GetTendia(){
-    return this->Tendia;
-}
-
-int Tension::GetTensys(){
-    return this->Tensys;
-}
-
-bool Tension::lireCapteur() {
-    this->Tensys = (int)distTensys(engineRandomTension);
-    this->Tendia = (int)disTendya(engineRandomTension);
-    this->TenP = (int)disTenP(engineRandomTension);
-    return true;
-}
-
-Tension::~Tension() noexcept { 
-    std::cout << " Dtor Tensions " << std::endl;
-}
-
+#endif /* Tension_hpp */
